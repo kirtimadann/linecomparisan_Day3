cff43693c0f68f904a746f24bcd4989d6da811cb
lunainfotec <kirtimadan1997@gmail.com>
2023-05-03 16:13:49 +0530

    added : welcome message

---
+++ src/com/bl/linecomparisan/LineCompare.java
@@ -0,0 +1,7 @@
+package com.bl.linecomparisan;
+
+public class LineCompare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
